02c79ae69f25b138bc9bf0b38e4feac046e10e59
Kilian Delcenserie <kilian.dlcsr92@gmail.com>
2025-02-13 09:26:33 +0100

    backoffice profile commit

--- back/router.js
+++ back/router.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import authRoutes from './routes/AuthRoutes.js';
+import user from './routes/Userroutes.js';
 
 const router = express.Router();
 
 // Regroupement des routes sous /auth
 router.use('/auth', authRoutes);
+router.use('/user', user);
 
 export default router;
